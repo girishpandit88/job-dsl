16b1dfd07d045f58c21fee0552f32decb202b0dd
Girish Pandit <girschol7@gmail.com>
2014-02-13 13:51:21 -0800

    add a downstream job2

--- jobDSL.groovy
+++ jobDSL.groovy
@@ -3,22 +3,23 @@ def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 branches.each { 
     def branchName = it.name
-    def downstreamUnityJob = job {
-	name "${project}-${branchName}.unity".replaceAll('/','-')
-	scm {
-	    git("git://github.com/${project}.git", branchName)
-	}
-	steps {
-		unity(Unity3d) {
-			
-		}
-	}
-    }
     job {
         name "${project}-${branchName}".replaceAll('/','-')
-        scm {
+        label('osx')
+	scm {
             git("git://github.com/${project}.git", branchName)
         }
+    def downstreamUnityJob = job {
+		name "${project}-${branchName}.unity".replaceAll('/','-')
+		scm {
+		    git("git://github.com/${project}.git", branchName)
+		}
+		steps {
+			unity(Unity3d) {
+				
+			}
+		}
+    }
 	publishers{
 		downstream(downstreamUnityJob, 'SUCCESS')
 	}
