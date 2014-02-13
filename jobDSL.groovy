f749ae7d0aeae809690e912ffce6beda5f5b3af4
Girish Pandit <girschol7@gmail.com>
2014-02-13 13:18:20 -0800

    Initial commit

---
+++ jobDSL.groovy
@@ -0,0 +1,16 @@
+def project = 'girishpandit88/StarTrooper'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each { 
+    def branchName = it.name
+    job {
+        name "${project}-${branchName}".replaceAll('/','-')
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        publisher{
+                downstream("${project}.unity", 'SUCCESS')
+        }
+        
+    }
+}
