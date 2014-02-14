fc4f5014419b94d09daebbb7efa49cfb35fc38b8
Girish Pandit <girschol7@gmail.com>
2014-02-13 16:32:11 -0800

    Modify the downstream jobs to pass the archived artifacts downstream7

--- jobDSL.groovy
+++ jobDSL.groovy
@@ -40,14 +40,14 @@ branches.each {
 		
 		configure { project ->
 			project / builders / 'hudson.plugins.copyartifact.CopyArtifact'(plugin: "copyartifact@1.28"){
-				project('downstreamUnityJob.name')
+				project(downstreamUnityJob.name)
 				filter('target/**')
 				selector 'hudson.plugins.copyartifact.SpecificBuildSelector'{
 					buildNumber("$UNITY_BUILD_NUMBER}")
 				}
 			}
 		}
-		
+
 		configure { project ->
 			project/ builders / 'au.com.rayh.XCodeBuilder'(plugin: 'xcode-plugin@1.4.1'){
 				cleanBeforeBuild('true')
